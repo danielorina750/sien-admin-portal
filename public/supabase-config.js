1b1d0e9c5894620288ecc02f19443153c88a3066
Daniel Orina <65708288+danielorina750@users.noreply.github.com>
2026-06-23 10:58:53 +0300

    Update supabase-config.js

--- public/supabase-config.js
+++ public/supabase-config.js
@@ -4,5 +4,5 @@ window.SIEN_SUPABASE_CONFIG = {
   url: 'https://uqxpdggzghdkmamectjr.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxeHBkZ2d6Z2hka21hbWVjdGpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0Mjk0MDUsImV4cCI6MjA5NzAwNTQwNX0.bN1HYfyavkvc-GAq1sgZofwltglrS9wsCrREFg-eqCU',
   bucket: 'project-images',
-  publicWebsiteUrl: 'https://sien-group.vercel.app'
+  publicWebsiteUrl: 'www.sien.co.ke'
 };
